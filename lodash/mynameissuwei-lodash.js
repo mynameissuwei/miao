256b032d4f5aad23936b9aa84d23e1d3875de300
suwei <asdfa>
2018-07-18 09:30:52 +0800

    damiaotask

--- lodash/mynameissuwei-lodash.js
+++ lodash/mynameissuwei-lodash.js
@@ -1,10 +1,10 @@
 var mynameissuwei = {
-  chunk: function(array, size) {
-    var myCars = []
-    for (var i = 0; i < array.length; i = i + size) {
-      myCars.push(array.slice(i, i + size))
+  chunk: function chunk(array, size) {
+    var array1 = []
+    for (var i = array.length; i > size; i--) {
+      array1.push(array.splice(i, 1))
     }
-    return myCars
-  }
+    return array1
+  },
 
 }
