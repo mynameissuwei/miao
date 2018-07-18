e75576f5a30c9e394038547f56819229f210e478
suwei <asdfa>
2018-07-18 22:28:40 +0800

    damiaotask

--- lodash/mynameissuwei-lodash.js
+++ lodash/mynameissuwei-lodash.js
@@ -7,7 +7,7 @@ var mynameissuwei = {
     return array1
   },
 
-  compact: function compact(array) {
+  compact: function (array) {
     return array.filter(x => x)
   },
 
@@ -19,24 +19,39 @@ var mynameissuwei = {
     return array
   },
 
-  difference: function (array,...value) {
-    function spread(array) {
-      var res = []
-      for(i = 0; i < array.length; i++) {
-        res.push(...array[i])
-      }
-      return res
+  reverse: function(array) {
+    var last = array.length - 1
+    for (var i = 0; i < last / 2; i++) {
+      [array[i],array[last - i]] = [array[last - i],array[i]]
     }
+    return array
+  },
+
+  slice function(array,start = 0, end = array.length - 1) {
     var res = []
-    value = spread(value)
-    w:for(i = 0; i < array.length; i++) {
-        for(j = 0; j < value.length; j++) {
-          if((Object.is( array[i],value[j] )) ) {
-            continue w
-          }
-        }
-      res.push(array[i])
+    for(start = 0; start <= end; start++) {
+      res.push(array[start])
     }
     return res
   },
+  // difference: function (array,...value) {
+  //   function spread(array) {
+  //     var res = []
+  //     for(i = 0; i < array.length; i++) {
+  //       res.push(...array[i])
+  //     }
+  //     return res
+  //   }
+  //   var res = []
+  //   value = spread(value)
+  //   w:for(i = 0; i < array.length; i++) {
+  //       for(j = 0; j < value.length; j++) {
+  //         if((Object.is( array[i],value[j] )) ) {
+  //           continue w
+  //         }
+  //       }
+  //     res.push(array[i])
+  //   }
+  //   return res
+  // },
 }
